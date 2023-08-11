a6bb1578104e561ee488a735f64f15bd04efdac1
rezwan2230 <rezwan2230@gmail.com>
2023-08-12 02:30:03 +0600

    added innerText & innerHtml

--- js/dom.js
+++ js/dom.js
@@ -1,16 +1,48 @@
-// console.log("Hello From JS")
+
 // console.log(document);
 // console.log(document.body);
 
+// const liCollection = document.getElementsByTagName("li")
+// for (const li of liCollection) {
+//     console.log(li.innerText);
+// }
+
+// const allheading = document.getElementsByTagName("h1")
+// for (const h1 of allheading) {
+//     // console.log(h1.innerText)
+// }
+
+// console.log(document.getElementById("places-title").innerText = "My tour list")
+
+// console.log(document.getElementsByClassName("important-places"))
+
+// const places = document.getElementsByClassName("important-places")
+// for(const place of places){
+//     console.log(place.innerText)
+// }
 
 
-const liCollection = document.getElementsByTagName("li")
-for (const li of liCollection) {
-    console.log(li.innerText);
-}
+// const fruitTitle = document.getElementById("fruits-title").innerText = "fruits change by JS"
+
+// console.log(document.getElementById("fruits-title"))
 
 
-const allheading = document.getElementsByTagName("h1")
-for (const h1 of allheading) {
-    console.log(h1.innerText)
-}
+// console.log(document.getElementsByClassName("important-places"))    
+// console.log(document.getElementsByClassName("other-places"))    
+
+
+
+//querySelector & querySelectorAll
+
+// const oneli = document.querySelector(".fruit-container li")
+// console.log(oneli)
+
+// const someli = document.querySelectorAll(".fruit-container li")
+// console.log(someli)
+// for(const li of someli){
+//     console.log(li.innerText)
+// }
+
+
+
+
